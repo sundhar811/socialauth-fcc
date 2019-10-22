52f6fb4d0a26f9ce4911011467c4942508273a1c
Glitch (sundhar811-socialauth-fcc) <none>
2019-10-22 09:57:38 +0000

    Implementation of Social Authentication

--- server.js
+++ server.js
@@ -56,11 +56,11 @@ mongo.connect(process.env.DATABASE, (err, db) => {
         *  ADD YOUR CODE BELOW
         */
       
+        app.route('/auth/github')
+          .get(passport.authenticate('github'))
       
-      
-      
-      
-      
+        app.route('/auth/github/callback')
+          .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => res.redirect('/profile'));
       
         /*
         *  ADD YOUR CODE ABOVE
